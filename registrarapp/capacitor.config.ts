d7270deeb8bcfc96563cf40d373bb17db89d6e4e
BurritoLopr <shadowcb@gmail.com>
2023-08-30 09:17:31 -0400

    primero

---
+++ registrarapp/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'registrarapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
