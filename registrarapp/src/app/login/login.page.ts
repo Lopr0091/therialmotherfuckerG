3df013a4f8fac4bb99d5f41a6c0709f258f97631
Midorisha <l.gomez2@duocuc.cl>
2023-09-04 20:53:11 -0300

    elimino un import roto

--- registrarapp/src/app/login/login.page.ts
+++ registrarapp/src/app/login/login.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 import { AlertController } from '@ionic/angular';
 import {UsuarioService} from'../usuario.service';
 @Component({
