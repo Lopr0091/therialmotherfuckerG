de9640abb5847fc1a8c4eb53b3b557ef2ddb4b92
Midorisha <l.gomez2@duocuc.cl>
2023-11-16 11:49:12 -0300

    se llena automaticamente al iniciar la aplicacion

--- registrarapp/src/app/admin/admin.page.ts
+++ registrarapp/src/app/admin/admin.page.ts
@@ -7,29 +7,36 @@ import { DbService } from '../db.service';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-  usuarios: any[]=[];
-  constructor(private dbService: DbService) { }
-  llenar(){
-      const id: number=2;
-      const nombre: string ='NombreUsuario2';
-      const clave: string ='clave2';
-      const correo: string ='correo1@ejemplo.com';
+  usuarios: any[] = [];
+
+  constructor(private dbService: DbService) {}
+
+  llenar() {
+    const id: number = 2;
+    const nombre: string = 'NombreUsuario2';
+    const clave: string = 'clave2';
+    const correo: string = 'correo1@ejemplo.com';
 
     this.dbService.createUser(id, nombre, clave, correo);
-    console.log('Usuario creado: ',this.dbService.getUserById(1));
+    console.log('Usuario creado: ', this.dbService.getUserById(1));
   }
-  ver(){
-    this.usuarios=[];
-    for (let id=1; ;id++){
+
+  ver() {
+    this.usuarios = [];
+    for (let id = 1; ; id++) {
       const usuario = this.dbService.getUserById(id);
-      if(!usuario){
+      if (!usuario) {
         break;
       }
       this.usuarios.push(usuario);
     }
   }
+
+  ionViewDidEnter() {
+    this.llenar();
+  }
+
   ngOnInit() {
     this.ver();
   }
-
 }
